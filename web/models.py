3d36d8763dc5e52c59d39b66c95fc3e24594e46e
Empathy Castro <empathy.castro@colorado.edu>
2023-02-20 16:32:46 -0700

    team-16

--- web/models.py
+++ web/models.py
@@ -1,10 +1,21 @@
-from web.db import db, BaseDocument, DataType
+from web.db import db, BaseDocument, DataType, ListType
 from datetime import datetime
 
 
-class Test(BaseDocument):
+class Vulnerability(BaseDocument):
     collection = db.test
     fields = {
-        "name": DataType(str, nullable=False),
-        "time": DataType(datetime, nullable=False)
+        "cve_id": DataType(str, nullable=False),
+        "created_at": DataType(datetime, nullable=False),
+        "updated_at": DataType(datetime, nullable=False),
+        "cvss": ListType(DataType(float), nullable=False),
+        "vendors": ListType(DataType(str), nullable=False),
+        "products": ListType(DataType(str), nullable=False),
+        "cwe_codes": ListType(DataType(int), nullable=False),
+        "cwe_names": ListType(DataType(str)),
+        "description": DataType(str, nullable=False),
+        "description_lang": DataType(str, nullable=False, default="en"),
+        "access_vector": DataType(str, nullable=False),
+        "access_complexity": DataType(str, nullable=False),
+        "access_authentication": DataType(str, nullable=False)
     }
