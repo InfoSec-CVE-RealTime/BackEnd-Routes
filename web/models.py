19f009b496757513c7171b178d53fd4814a0ef53
Empathy Castro <empathy.castro@colorado.edu>
2023-02-27 14:45:30 -0700

    Models based on data in mongo

--- web/models.py
+++ web/models.py
@@ -1,21 +1,48 @@
-from web.db import db, BaseDocument, DataType, ListType
+from web.db import db, BaseDocument, DataType
 from datetime import datetime
 
 
-class Vulnerability(BaseDocument):
-    collection = db.test
+class CVE(BaseDocument):
+    collection = db.cve
     fields = {
         "cve_id": DataType(str, nullable=False),
-        "created_at": DataType(datetime, nullable=False),
-        "updated_at": DataType(datetime, nullable=False),
-        "cvss": ListType(DataType(float), nullable=False),
-        "vendors": ListType(DataType(str), nullable=False),
-        "products": ListType(DataType(str), nullable=False),
-        "cwe_codes": ListType(DataType(int), nullable=False),
-        "cwe_names": ListType(DataType(str)),
-        "description": DataType(str, nullable=False),
-        "description_lang": DataType(str, nullable=False, default="en"),
+        "pub_date": DataType(datetime, nullable=False),
+        "mod_date": DataType(datetime, nullable=False),
+        "cvss": DataType(float, nullable=False),
+        # "vendors": ListType(DataType(str), nullable=False),
+        # "products": ListType(DataType(str), nullable=False),
+        "cwe_code": DataType(int, nullable=False),
+        "cwe_name": DataType(str, nullable=False),
+        "summary": DataType(str, nullable=False),
         "access_vector": DataType(str, nullable=False),
         "access_complexity": DataType(str, nullable=False),
-        "access_authentication": DataType(str, nullable=False)
+        "access_authentication": DataType(str, nullable=False),
+        "impact_availability": DataType(str, nullable=False),
+        "impact_confidentiality": DataType(str, nullable=False),
+        "impact_integrity": DataType(str, nullable=False)
+    }
+
+
+class Product(BaseDocument):
+    collection = db.products
+    fields = {
+        "cve_id": DataType(str, nullable=False),
+        "vulnerable_product": DataType(str, nullable=False)
+    }
+
+
+class VendorProduct(BaseDocument):
+    collection = db.vendor_products
+    fields = {
+        "index": DataType(str, nullable=False),
+        "vendor": DataType(str, nullable=False),
+        "product": DataType(str, nullable=False)
+    }
+
+
+class Vendor(BaseDocument):
+    collection = db.vendors
+    fields = {
+        "cve_id": DataType(str, nullable=False),
+        "vendor": DataType(str, nullable=False)
     }
