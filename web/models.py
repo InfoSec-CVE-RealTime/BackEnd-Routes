0816554cbee0ed0039c9b7cf6f1f0699265c038a
Empathy Castro <empathy.castro@colorado.edu>
2023-03-06 17:08:09 -0700

    remove suggestions

--- web/models.py
+++ web/models.py
@@ -1,4 +1,4 @@
-from web.db import db, BaseDocument, DataType, ReferenceType
+from web.db import db, BaseDocument, DataType
 from datetime import datetime
 
 
@@ -9,8 +9,6 @@ class CVE(BaseDocument):
         "pub_date": DataType(datetime, nullable=False),
         "mod_date": DataType(datetime, nullable=False),
         "cvss": DataType(float, nullable=False),
-        # "vendors": ListType(DataType(str), nullable=False),  # suggested add
-        # "products": ListType(DataType(str), nullable=False),  # suggested add
         "cwe_code": DataType(int, nullable=False),
         "cwe_name": DataType(str, nullable=False),
         "summary": DataType(str, nullable=False),
@@ -26,23 +24,23 @@ class CVE(BaseDocument):
 class Product(BaseDocument):
     collection = db.products
     fields = {
-        "cve_id": DataType(str, nullable=False),  # suggested remove
-        "vulnerable_product": DataType(str, nullable=False)  # suggested rename to "product_name"
+        "cve_id": DataType(str, nullable=False),
+        "vulnerable_product": DataType(str, nullable=False)
     }
 
 
 class VendorProduct(BaseDocument):
     collection = db.vendor_products
     fields = {
-        "": DataType(int, nullable=False),  # suggested remove
-        "vendor": DataType(str, nullable=False),  # suggested rename to "vendor_name"
-        "product": DataType(str, nullable=False)  # suggested rename to "product_name"
+        "": DataType(int, nullable=False),
+        "vendor": DataType(str, nullable=False),
+        "product": DataType(str, nullable=False)
     }
 
 
 class Vendor(BaseDocument):
     collection = db.vendors
     fields = {
-        "cve_id": DataType(str, nullable=False),  # suggested remove
-        "vendor": DataType(str, nullable=False)  # suggested rename to "vendor_name"
+        "cve_id": DataType(str, nullable=False),
+        "vendor": DataType(str, nullable=False)
     }
