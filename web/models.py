d09efa501763a6a49a64c2b69d4e1a9e691e37e8
Empathy Castro <empathy.castro@colorado.edu>
2023-03-11 22:17:09 -0700

    finish api 2

--- web/models.py
+++ web/models.py
@@ -1,6 +1,8 @@
 from web.db import db, BaseDocument, DataType
 from datetime import datetime
 
+MIN_DATE = datetime(1988, 1, 1)
+
 
 class CVE(BaseDocument):
     collection = db.cve
@@ -29,6 +31,55 @@ class CVE(BaseDocument):
             {"$limit": page_size}
         ])
         return list(cves) if as_dicts else [cls(cve) for cve in cves]
+
+    @classmethod
+    def get_binned_by_field(cls, field, min_date, max_date, bin_size):
+        date_format = cls.get_bin_aggregate_date_format(bin_size)
+        cves = cls.collection.aggregate([
+            {"$match": {"pub_date": {"$gte": min_date, "$lte": max_date}}},
+            {"$project": {"pub_date": 1, field: 1}},
+            {"$group": {
+                "_id": {
+                    "field": "$" + field,
+                    "date": {"$dateToString": {"format": date_format, "date": "$pub_date"}}
+                },
+                "count": {"$sum": 1}
+            }},
+            {"$sort": {"_id.date": 1}}
+        ])
+        data = cls.collect_data_by_bin(list(cves), min_date, max_date, bin_size)
+        return data
+
+    @classmethod
+    def collect_data_by_bin(cls, cves, min_date, max_date, bin_size):
+        data = []
+        date_format = cls.get_bin_aggregate_date_format(bin_size)
+        current_date = min_date
+        while current_date <= max_date:
+            data_date = current_date.strftime(date_format)
+            current_bin = {"date": current_date.strftime("%Y-%m-%d")}
+            for cve in cves:
+                if cve["_id"]["date"] == data_date and cve["_id"].get("field"):
+                    current_bin[cve["_id"]["field"]] = cve["count"]
+            data.append(current_bin)
+            current_date = cls.get_next_bin_date(current_date, bin_size)
+        return data
+
+    @classmethod
+    def get_bin_aggregate_date_format(cls, bin_size):
+        if bin_size == "month":
+            return "%Y-%m"
+        else:  # bin_size == "year"
+            return "%Y"
+
+    @classmethod
+    def get_next_bin_date(cls, current_date, bin_size):
+        if bin_size == "year":
+            return current_date.replace(year=current_date.year + 1)
+        else:  # bin_size == "month"
+            next_month = current_date.month + 1 if current_date.month < 12 else 1
+            next_year = current_date.year + 1 if next_month == 1 else current_date.year
+            return current_date.replace(year=next_year, month=next_month)
 
 
 class Product(BaseDocument):
--- web/routes.py
+++ web/routes.py
@@ -1,7 +1,7 @@
 import flask
 from web import app
 from datetime import datetime
-from web.models import CVE
+from web.models import CVE, MIN_DATE
 from web.db import get_json_compatible
 
 
@@ -10,17 +10,41 @@ def home():
     return flask.jsonify({"message": "Hello World!"})
 
 
-@app.route("/api/v1.0/top_cves")
+@app.route("/api/v1.0/top_cves")  # API ROUTE 1
 def top_cves():
-    min_date = get_arg("min_date", default=datetime(1970, 1, 1), coerce_type=datetime)
+    min_date = get_arg("min_date", default=MIN_DATE, coerce_type=datetime)
     max_date = get_arg("max_date", default=datetime.now(), coerce_type=datetime)
     page = get_arg("page", default=1, coerce_type=int) - 1
     page_size = get_arg("page_size", default=10, coerce_type=int)
+    if page < 0:
+        page = 0
+    if page_size > 500:
+        page_size = 500
+    if page_size < 1:
+        page_size = 1
     cves = CVE.get_top_cves(min_date, max_date, page, page_size)
     return flask.jsonify(get_json_compatible(cves))
 
 
-def get_arg(property_name, default=None, coerce_type=None):
-    if property_name in flask.request.args:
-        return flask.request.args.get(property_name, type=coerce_type)
+@app.route("/api/v1.0/access_complexity")  # API ROUTE 2
+def access_complexity():
+    min_date = get_arg("min_date", default=MIN_DATE, coerce_type=datetime)
+    max_date = get_arg("max_date", default=datetime.now(), coerce_type=datetime)
+    bin_size = get_arg("bin_size", default="year", choices=("month", "year"))
+    data = CVE.get_binned_by_field("access_complexity", min_date, max_date, bin_size)
+    return flask.jsonify(get_json_compatible(data))
+
+
+def get_arg(arg_name, default=None, coerce_type=None, choices=()):
+    if arg_name in flask.request.args:
+        try:
+            if coerce_type is datetime:
+                value = datetime.strptime(flask.request.args.get(arg_name), "%Y-%m-%d")
+            else:
+                value = flask.request.args.get(arg_name, type=coerce_type)
+        except (TypeError, ValueError):
+            return default
+        if choices and value not in choices:
+            return default
+        return value
     return default
